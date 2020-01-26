7e85fb58c0b6b2d61d857856ef080c2693ee6dd5
sngc1 <sngc1@users.noreply.github.com>
2020-01-26 13:38:06 +0900

    Fix file extention from gz to xz

--- get_nncase.sh
+++ get_nncase.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 mkdir -p ncc
 mkdir -p workspace
 mkdir -p images
 mkdir -p log
-cd ncc
+
+pushd ncc
 wget https://github.com/kendryte/nncase/releases/download/v0.1.0-rc5/ncc-linux-x86_64.tar.xz
-tar -Jxf ncc-linux-x86_64.tar.gz
-rm ncc-linux-x86_64.tar.gz
+tar -Jxf ncc-linux-x86_64.tar.xz
+rm ncc-linux-x86_64.tar.xz
 echo "download nncase ok!"
+popd
